f61ced78337f6d0940b30ad7550d04c53ec486e4
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-12 21:00:06 +0300

    commit tree a6498a7c3032625bedc291cdf0e03ba7bb8e4aa8

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+function findMatching(array, name) {
+    let result = array.filter(item => item.toLowerCase() === name.toLowerCase())
+    return result
+}
+
+
+function fuzzyMatch(array, name){
+    let result = array.filter(item => item.slice(0, 1) === name.slice(0, 1));
+
+    return result
+}
+
+
+const drivers = [
+    {
+      name: 'Gako',
+      hometown: 'Canada' },
+    {
+      name: 'John',
+      hometown: 'Dubai' } ,
+    {
+      name: 'James',
+      hometown: 'Lavington' },
+    {
+      name: 'Sarah',
+      hometown: 'Downnhome' },
+    {
+      name: 'Billaxon',
+      hometown: 'London' }
+  ];
+
+function matchName(drivers, name){
+    let result = drivers.filter(obj => obj.name === name);
+
+    return result
+}
